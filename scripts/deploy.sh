fb79d86b6ea3b18a35eec59d63bf1d99779be9cd
joeySchentrup <j03y@live.com>
2019-01-30 20:59:00 -0500

    Fixed the production run scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,4 +6,4 @@ set -xe
 # setup ssh agent, git config and remote
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/travis_rsa
-ssh travis@157.230.182.92 "cd /var/www/CatholicGatorsAdminApp && git pull && docker-compose up -f docker-compose-testing.yml --build"
+ssh travis@157.230.182.92 "cd /var/www/CatholicGatorsAdminApp && git pull && docker-compose -f docker-compose-production.yml up --build"
