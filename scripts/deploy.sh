abd9fc67731ad78787b5b84ab0fda531dbd3c65f
joeySchentrup <j03y@live.com>
2019-01-31 14:45:05 -0500

    Fixed bash script error

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,4 +6,7 @@ set -xe
 # setup ssh agent, git config and remote
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/travis_rsa
-ssh travis@157.230.182.92 "cd /var/www/CatholicGatorsAdminApp && git pull && docker-compose -f docker-compose-production.yml up --build"
+ssh root@157.230.182.92 "\
+    sudo su travis && \
+    cd /var/www/CatholicGatorsAdminApp && \
+    git pull && docker-compose -f docker-compose-production.yml up --build"
