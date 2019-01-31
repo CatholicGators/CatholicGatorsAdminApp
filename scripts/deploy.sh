53e425fa13a3b5c72f00391708681796cca8c647
joeySchentrup <j03y@live.com>
2019-01-30 19:42:36 -0500

    Change deployment scheme
    * Edit travis CI file to deploy to testing enviroment on merge to master
    
    * Create deployment scripts to deploy to DO
    
    * Moved env vars to seperate docker compose files

---
+++ scripts/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# print outputs and exit on first failure
+set -xe
+
+# setup ssh agent, git config and remote
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/travis_rsa
+ssh travis@157.230.182.92 "cd /var/www/CatholicGatorsAdminApp && git pull && docker-compose up -f docker-compose-testing.yml --build"
