dac9b8050f87e0439dcf962e63585e50bbddff24
joeySchentrup <j03y@live.com>
2019-01-31 15:45:57 -0500

    Clean up
    * Made it so the pre-deployment set up only happens when a deployment will
    
    * Also, detached container from shell so build completes when container is up

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,5 +6,5 @@ set -xe
 # setup ssh agent, git config and remote
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/travis_rsa
-ssh travis@157.230.182.92 "cd /var/www/CatholicGatorsAdminApp && git pull && docker-compose -f docker-compose-production.yml up --build"
+ssh travis@157.230.182.92 "cd /var/www/CatholicGatorsAdminApp && git pull && docker-compose -f docker-compose-production.yml up -d --build"
 
