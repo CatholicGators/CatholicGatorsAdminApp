a79ac5b8304038be80436e3a9e126779c5ba00e4
joeySchentrup <j03y@live.com>
2019-01-31 15:28:43 -0500

    Fix line endings

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,5 @@ set -xe
 # setup ssh agent, git config and remote
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/travis_rsa
-ssh root@157.230.182.92 "\
-    sudo su travis && \
-    cd /var/www/CatholicGatorsAdminApp && \
-    git pull && docker-compose -f docker-compose-production.yml up --build"
+ssh travis@157.230.182.92 "cd /var/www/CatholicGatorsAdminApp && git pull && docker-compose -f docker-compose-production.yml up --build"
+
