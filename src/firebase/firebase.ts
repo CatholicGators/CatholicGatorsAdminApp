0a38ce92f66fef936219d9bda8af9bcf6cd3c55f
RyanClementsHax <ryanclementsuf@gmail.com>
2019-01-31 14:01:22 -0500

    Removed useless stuff, need to make creds work with docker

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,26 +1,9 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
+import clientCreds from '../../credentials/client';
 
-const prodConfig = {
-  apiKey: "AIzaSyAy_mXfE3IRoX_G3NpHazneLc8AXYI_aCI",
-  authDomain: "catholic-gators.firebaseapp.com",
-  databaseURL: "https://catholic-gators.firebaseio.com",
-  projectId: "catholic-gators",
-  storageBucket: "catholic-gators.appspot.com",
-  messagingSenderId: "749371573275"
-};
-
-const devConfig = {
-  apiKey: "AIzaSyDCDeYDMBWkNDv6vYHvliK75FZIYeui8go",
-  authDomain: "catholic-gators-dev.firebaseapp.com",
-  databaseURL: "https://catholic-gators-dev.firebaseio.com",
-  projectId: "catholic-gators-dev",
-  storageBucket: "catholic-gators-dev.appspot.com",
-  messagingSenderId: "1019003476611"
-};
-
-const config = process.env.NODE_ENV === "production" ? prodConfig : devConfig;
+const config = clientCreds[process.env.NODE_ENV];
 
 if (!firebase.apps.length) {
   firebase.initializeApp(config);
