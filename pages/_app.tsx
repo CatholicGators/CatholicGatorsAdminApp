c30de6be80e781a4a3f0fc7fe6198b8f51dbe52d
Ryan Clements <ryanclementshax@gmail.com>
2019-02-04 17:10:19 -0500

    Re did commit for auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
+import { Provider } from "react-redux";
 import JssProvider from 'react-jss/lib/JssProvider';
+
+import Router from 'next/router';
 import App, { Container } from 'next/app';
 import Head from 'next/head';
+import withRedux from "next-redux-wrapper";
+import NProgress from 'nprogress';
 
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-import Header from "../components/Header";
-import getPageContext from '../src/getPageContext';
+import Header from "../src/components/Layout/Header";
+import getPageContext from '../src/material/getPageContext';
+import initStore from "../src/store";
 
-export default class CatholicGatorsAdminApp extends App {
+Router.onRouteChangeStart = () => NProgress.start();
+Router.onRouteChangeComplete = () => NProgress.done();
+Router.onRouteChangeError = () => NProgress.done();
+
+class CatholicGatorsAdminApp extends App {
   public pageContext;
 
-  constructor(props) {
+  constructor(public props: any) {
     super(props);
     this.pageContext = getPageContext();
   }
@@ -26,11 +36,39 @@ export default class CatholicGatorsAdminApp extends App {
   }
 
   render() {
-    const { Component, pageProps } = this.props;
+    const { Component, pageProps, store } = this.props;
     return (
       <Container>
         <Head>
           <title>CG Admin</title>
+          <style>{`
+            * {
+              box-sizing: border-box;
+            }
+
+            html, body {
+              height: 100%;
+              width: 100%;
+            }
+
+            /* Make clicks pass-through */
+            #nprogress {
+              pointer-events: none;
+            }
+
+            /* color and other parameter */
+            #nprogress .bar {
+              background: #1976D2;
+
+              position: fixed;
+              z-index: 1000;
+              top: 0;
+              left: 0;
+
+              width: 100%;
+              height: 2px;
+            }
+          `}</style>
         </Head>
         {/* Wrap every page in Jss and Theme providers */}
         <JssProvider
@@ -45,13 +83,19 @@ export default class CatholicGatorsAdminApp extends App {
           >
             {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
             <CssBaseline />
-            <Header></Header>
             {/* Pass pageContext to the _document though the renderPage enhancer
                 to render collected styles on server-side. */}
-            <Component pageContext={this.pageContext} {...pageProps} />
+                
+            <Provider store={store}>
+              <Header>
+                <Component pageContext={this.pageContext} {...pageProps}/>
+              </Header>
+            </Provider>
           </MuiThemeProvider>
         </JssProvider>
       </Container>
     );
   }
 }
+
+export default withRedux(initStore)(CatholicGatorsAdminApp);
--- server/index.ts
+++ server/index.ts
@@ -1,24 +1,74 @@
 import * as express from 'express';
 import * as next from 'next';
+import * as bodyParser from 'body-parser';
+import * as session from 'express-session';
+import * as admin from 'firebase-admin';
 
-const port:number = parseInt(process.env.PORT, 10) || 3000;
-const dev:boolean = process.env.NODE_ENV !== 'production';
-const app:next.Server = next({ dev });
-const handle:Function = app.getRequestHandler();
+const FirestoreStore = require( 'firestore-store' )(session);
 
-if(!process.env.NODE_ENV) {
-    throw new Error('ERROR: NODE_ENV is set to ' + process.env.NODE_ENV);
-}
+const serverCreds = require(`../${process.env.SERVICE_ACCOUNT_PRIVATE_KEY_FILE_NAME}`);
+const port: number = parseInt(process.env.PORT, 10) || 3000;
+const dev: boolean = process.env.NODE_ENV !== 'production';
+const app: next.Server = next({ dev });
+const handle: Function = app.getRequestHandler();
+const firebase = admin.initializeApp({
+    credential: admin.credential.cert(serverCreds)
+});
+
+const firestore = firebase.firestore();
+firestore.settings({
+    timestampsInSnapshots: true
+});
 
 app.prepare()
     .then(() => {
-        const server:express.Express = express();
+        const server = express();
 
-        server.get('*', (req:Express.Request, res:Express.Response) => {
-            return handle(req, res);
+        server.use(bodyParser.json());
+        server.use(
+            session({
+                store: new FirestoreStore({
+                    database: firestore
+                }),
+                secret: 'geheimnis',
+                saveUninitialized: true,
+                resave: false,
+                rolling: true,
+                httpOnly: true,
+                cookie: { maxAge: 604800000 } // week
+            })
+        );
+
+        server.use((req: any, _, next) => {
+            req.firebaseServer = firebase;
+            next();
         });
 
-        server.listen(port, (err:Error) => {
+        server.post('/api/login', (req:any , res) => {
+            if (!req.body) return res.sendStatus(400)
+        
+            const token = req.body.token
+            firebase
+                .auth()
+                .verifyIdToken(token)
+                .then(decodedToken => {
+                    req.session.decodedToken = decodedToken
+                    return decodedToken
+                })
+                .then(decodedToken => res.json({ status: true, decodedToken }))
+                .catch(error => res.json({ error }))
+        })
+        
+        server.post('/api/logout', (req:any, res) => {
+            req.session.decodedToken = null
+            res.json({ status: true })
+        })
+        
+        server.get('*', (req, res) => {
+            return handle(req, res)
+        })
+
+        server.listen(port, (err) => {
             if (err) throw err;
             console.log(`> Ready on http://localhost:${port}`);
         });
