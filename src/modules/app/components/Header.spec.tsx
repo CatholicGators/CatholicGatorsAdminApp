be4b4a961c3b719673a878b260655f2c2b056aa7
Ryan Clements <ryanclementshax@gmail.com>
2019-02-25 12:26:56 -0500

    Got working test

--- src/modules/app/components/Header.spec.tsx
+++ src/modules/app/components/Header.spec.tsx
@@ -1,36 +1,25 @@
 import React from 'react'
 import { shallow } from 'enzyme';
-import configureStore from 'redux-mock-store';
 
 import Header from './Header';
 
-describe('Header', () => {
-    const initialState = {
-        user: null
-    };
-    const mockStore = configureStore();
-    let props, wrapper, store;
+describe('Header.tsx', () => {
+    let props, wrapper;
 
     beforeEach(() => {
-        store = mockStore(initialState);
         props = {
+            user: null,
             signOut: jest.fn(),
             googleSignIn: jest.fn()
         };
-        wrapper = shallow(<Header store={store}/>);
+        wrapper = shallow(<Header {...props} />);
     });
 
     it('should match snapshot', () => {
         expect(wrapper).toMatchSnapshot();
     });
-
-    describe('handleLogin()', () => {
-        it('should call googleSignIn()', () => {
-            const component = wrapper.dive();
-
-            component.find('#login-btn').simulate('click');
-
-            expect(store.getActions()).toBe('');
-        });
+    
+    it('should show the login button when user is null', () => {
+        expect(wrapper.exists('#login-btn'));
     });
 });
