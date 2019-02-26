e95e6da5225d89338ca153b3ec205a9e8c505ce0
Ryan Clements <ryanclementshax@gmail.com>
2019-02-25 21:54:36 -0500

    Finished component tests

--- src/modules/app/components/Header.spec.tsx
+++ src/modules/app/components/Header.spec.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { shallow } from 'enzyme';
 
-import Header from './Header';
+import { Header } from './Header';
 
-describe('Header.tsx', () => {
+describe('Header', () => {
     let props, wrapper;
 
     beforeEach(() => {
         props = {
-            user: null,
+            user: {},
             signOut: jest.fn(),
             googleSignIn: jest.fn()
         };
@@ -19,7 +19,51 @@ describe('Header.tsx', () => {
         expect(wrapper).toMatchSnapshot();
     });
     
-    it('should show the login button when user is null', () => {
-        expect(wrapper.exists('#login-btn'));
+    it('should show the avatar and hide the login button when user is not null', () => {
+        expect(wrapper.exists('#login-btn')).toBe(false);
+        expect(wrapper.exists('#avatar-btn')).toBe(true);
+    });
+
+    describe('login btn', () => {
+        beforeEach(() => {
+            wrapper.setProps({ user: null });
+        });
+
+        it('should show the login button and hide the avatar when user is null', () => {
+            expect(wrapper.exists('#login-btn')).toBe(true);
+            expect(wrapper.exists('#avatar-btn')).toBe(false);
+        });
+
+        it('calls googleSignIn()', () => {
+            wrapper.find('#login-btn').simulate('click');
+
+            expect(props.googleSignIn).toHaveBeenCalled();
+        });
+    });
+
+    describe('menu', () => {
+        const testTarget = 'test';
+
+        beforeEach(() => {
+            wrapper.find('#avatar-btn').simulate('click', { currentTarget: testTarget });
+        });
+
+        it('clicking avatar sets anchorEl', () => {
+            expect(wrapper.state('anchorEl')).toBe(testTarget);
+        });
+
+        describe('logout', () => {
+            it('closes the menu', () => {
+                wrapper.find('#logout').simulate('click');
+
+                expect(wrapper.state('anchorEl')).toBe(null);
+            });
+
+            it('calls signOut()', () => {
+                wrapper.find('#logout').simulate('click');
+
+                expect(props.signOut).toHaveBeenCalled();
+            });
+        });
     });
 });
