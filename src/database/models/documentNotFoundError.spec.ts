00068ec20f2de44727f224a94f1efcdc29c430ea
Michael Chan-Pong <Michael.Chan-Pong@outlook.com>
2019-03-06 18:20:53 -0500

    removed unused part of DocumentNotFoundError constructor

--- src/database/models/documentNotFoundError.spec.ts
+++ src/database/models/documentNotFoundError.spec.ts
@@ -1,11 +1,6 @@
 import DocumentNotFoundError from './documentNotFoundError';
 
 describe('DocumentNotFoundError', () => {
-    it('gives the default message when no document ID is passed', () => {
-        const err = new DocumentNotFoundError();
-        expect(err.message).toEqual('Document does not exist in database');
-    });
-
     it('gives a custom message when a document ID is passed', () => {
         const err = new DocumentNotFoundError('docId');
         expect(err.message).toEqual('Document docId does not exist in database');
--- src/database/models/documentNotFoundError.ts
+++ src/database/models/documentNotFoundError.ts
@@ -1,10 +1,7 @@
 export default class DocumentNotFoundError extends Error {
-    constructor(document?: string) {
+    constructor(document: string) {
         super();
         this.name = "DocumentNotFoundError";
-        if (document)
-            this.message = `Document ${document} does not exist in database`;
-        else
-            this.message = 'Document does not exist in database';
+        this.message = `Document ${document} does not exist in database`;
     }
 }
