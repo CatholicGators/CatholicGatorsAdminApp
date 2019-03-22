93005e0ba18f606426b446680a66cb589d791933
Ryan Clements <ryanclementshax@gmail.com>
2019-03-22 13:35:13 -0400

    Added route link hiding

--- src/modules/app/components/Header/Header.spec.tsx
+++ src/modules/app/components/Header/Header.spec.tsx
@@ -1,28 +1,62 @@
 import React from 'react'
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme'
+import { MemoryRouter } from 'react-router-dom'
 
-import { Header } from './Header';
+import { Header, menuLinks } from './Header'
 
 describe('Header', () => {
-    let props, wrapper;
+    let wrapper, props
 
     beforeEach(() => {
         props = {
-            user: {},
+            user: {
+                isAdmin: true
+            },
             location: {
                 pathname: '/'
             },
             signOut: jest.fn(),
             googleSignIn: jest.fn()
-        };
-        wrapper = shallow(<Header {...props} />);
-    });
+        }
+        wrapper = shallow(<Header {...props} />)
+    })
 
     it('should match snapshot', () => {
-        expect(wrapper).toMatchSnapshot();
-    });
+        expect(wrapper).toMatchSnapshot()
+    })
 
     it('should have the drawer not open by default', () => {
-        expect(wrapper.state('drawerOpen')).toBe(false);
-    });
-});
+        expect(wrapper.state('drawerOpen')).toBe(false)
+    })
+
+    it('renders all links when user is an admin', () => {
+        const dom = mount(
+            <MemoryRouter>
+                <div>
+                    <Header {...props} />
+                </div>
+            </MemoryRouter>
+        )
+
+        menuLinks.forEach(link => expect(dom.exists(`NavLink[to='${link.href}']`)).toBe(true))
+    })
+
+    it('renders only unAuthorized links when user is not an admin', () => {
+        props = {
+            ...props,
+            user: {
+                ...props.user,
+                isAdmin: false
+            }
+        }
+        const dom = mount(
+            <MemoryRouter>
+                <div>
+                    <Header {...props} />
+                </div>
+            </MemoryRouter>
+        )
+
+        menuLinks.forEach(link => expect(dom.exists(`NavLink[to='${link.href}']`)).toBe(!link.needsAuthorization))
+    })
+})
