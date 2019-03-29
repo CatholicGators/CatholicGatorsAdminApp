7bd17506a937fbced2e2e95d72c4ac49a655eaf2
Ryan Clements <ryanclementshax@gmail.com>
2019-03-29 13:19:57 -0400

    Added UserTableRow tests

--- src/modules/admin/components/UserTableRow/UserTableRow.spec.tsx
+++ src/modules/admin/components/UserTableRow/UserTableRow.spec.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme';
 import { UserTableRow } from './UserTableRow';
+import { Checkbox, Switch } from '@material-ui/core';
 
 describe('UserTableRow', () => {
     let wrapper, props;
@@ -8,6 +9,7 @@ describe('UserTableRow', () => {
     beforeEach(() => {
         props = {
             user: {
+                id: 'test',
                 data: {
                     photoUrl: ''
                 }
@@ -22,5 +24,25 @@ describe('UserTableRow', () => {
 
     it('matches snapshot', () => {
         expect(wrapper).toMatchSnapshot()
+    })
+
+    it('calls handleSelect with user.id when the checkbox is clicked', () => {
+        wrapper.find(Checkbox).simulate('click')
+
+        expect(props.handleSelect).toHaveBeenCalledWith(props.user.id)
+    })
+
+    it('calls handleApproveToggle with the user and the checked value when the approve switched is toggled', () => {
+        const checked = true
+        wrapper.find(Switch).first().simulate('change', {target: {checked}}, checked)
+
+        expect(props.handleApproveToggle).toHaveBeenCalledWith(props.user, checked)
+    })
+
+    it('calls handleAuthorizeToggle with the user and the checked value when the authorize switch is toggled', () => {
+        const checked = true
+        wrapper.find(Switch).at(1).simulate('change', {target: {checked}}, checked)
+
+        expect(props.handleAuthorizeToggle).toHaveBeenCalledWith(props.user, checked)
     })
 })
