61ce54fe9a190fe90b32b2b3fd3b1ed58250d85a
Ryan Clements <ryanclementshax@gmail.com>
2019-04-08 16:25:32 -0400

    Made table

--- src/modules/contact/pages/myContacts.tsx
+++ src/modules/contact/pages/myContacts.tsx
@@ -1,8 +1,35 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { getContacts } from '../../../redux/actions/contact/contactActions';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { getContacts } from '../../../redux/actions/contact/contactActions'
+import {
+  Theme,
+  createStyles,
+  withStyles,
+  Paper,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Toolbar,
+  Typography,
+  Checkbox
+} from '@material-ui/core'
+
+const styles = (theme: Theme) => createStyles({
+  tableWrapper: {
+    maxWidth: '1000px',
+    marginTop: '50px',
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  },
+  tableCard: {
+    overflowX: 'auto'
+  }
+})
 
 type Props = {
+    classes: any,
     contacts: any,
     getContacts: () => void
 }
@@ -13,10 +40,39 @@ export class MyContacts extends Component<Props> {
     }
 
     render() {
+        const { classes, contacts } =  this.props
+    
         return (
-            <div>
-                <h1>My Contacts</h1>
-                <pre>{JSON.stringify(this.props.contacts, null, 2)}</pre>
+            <div className={classes ? classes.tableWrapper : null}>
+                <Paper className={classes ? classes.tableCard : null}>
+                    <Toolbar>
+                        <Typography variant="h6">
+                            My Contacts
+                        </Typography>
+                    </Toolbar>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell>Phone Number</TableCell>
+                                <TableCell>Email</TableCell>
+                                <TableCell>Called</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                        {contacts ? contacts.map(contact => (
+                            <TableRow hover key={contact.id}>
+                                <TableCell>{`${contact.firstName} ${contact.lastName}`}</TableCell>
+                                <TableCell>{contact.phoneNumber}</TableCell>
+                                <TableCell>{contact.email}</TableCell>
+                                <TableCell>
+                                    <Checkbox checked={contact.called} />
+                                </TableCell>
+                            </TableRow>
+                        )) : null}
+                        </TableBody>
+                    </Table>
+                </Paper>
             </div>
         );
     }
@@ -28,6 +84,6 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
     getContacts: () => dispatch(getContacts())
-});
+})
 
-export default connect(mapStateToProps, mapDispatchToProps)(MyContacts);
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(MyContacts))
