63a6f62955b6cc00ba8c1c68dc7ffc7c0a7e4fcb
joeySchentrup <j03y@live.com>
2019-04-09 15:07:17 -0400

    Fix merge typo

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import authReducer from './auth/authReducer';
-import adminReducer from './admin/adminReducer';
+import adminReducer from './admin/adminReducer'
+import contactFormReducer from './contactForm/contactFormReducer';
 
 export default combineReducers({
   auth: authReducer,
