d7b6070859d8fa0e252519ca410345f61f859272
joeySchentrup <j03y@live.com>
2019-04-13 10:50:28 -0400

    Move env var into the shell

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,5 +7,6 @@ set -xe
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/travis_rsa
 ssh travis@157.230.182.92 "cd /var/www/CatholicGatorsAdminApp \
+    && export NODE_ENV=production \
     && git pull \
-    && docker-compose up -e NODE_ENV=production -d --build --force-recreate"
+    && docker-compose up -d --build --force-recreate"
