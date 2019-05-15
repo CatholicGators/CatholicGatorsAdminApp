17d21e58f8add560c6cd7774f01fc769502ca2a9
Ryan Clements <ryanclementshax@gmail.com>
2019-05-14 21:53:06 -0400

    Added some admin tests

--- src/modules/admin/pages/Admin.spec.tsx
+++ src/modules/admin/pages/Admin.spec.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 import { shallow } from 'enzyme';
 
 import { Admin } from './Admin'
-import { TableHead, Checkbox } from '@material-ui/core';
+import {
+    TableHead,
+    Checkbox,
+    CircularProgress,
+    TableRow
+} from '@material-ui/core';
 
 describe('Admin', () => {
     let props, wrapper
@@ -41,19 +46,42 @@ describe('Admin', () => {
 
     it('shows the circular progress when run loading the users', () => {
         wrapper.setProps({ users: undefined })
-        expect(wrapper.find('#loading-spinner').exists()).toBe(true)
+        expect(wrapper.find(CircularProgress).exists()).toBe(true)
     })
 
-    it('sets the selected array to contain all of the ids when the table head checkbox is clicked', () => {
-        const header = wrapper.find(TableHead)
-        const checkbox = header.find(Checkbox)
+    describe('handleSelectAll()', () => {
+        it('sets the selected array to contain all of the ids when the table head checkbox is clicked', () => {
+            const header = wrapper.find(TableHead)
+            const checkbox = header.find(Checkbox)
+    
+            checkbox.simulate('change', { target: { checked: true } })
+    
+            const selected = wrapper.state().selected
+            expect(selected.length).toEqual(props.users.length)
+            for(var i = 0; i < props.users.length; i++) {
+                expect(selected).toContain(props.users[i].id)
+            }
+        })
+    })
 
-        checkbox.simulate('change', { target: { checked: true } })
+    describe('handleSelect(id)', () => {
+        it('adds the id to the selected field when id is not originally in the array', () => {
+            const id = props.users[1].id
+            wrapper.instance().handleSelect(id)
+            expect(wrapper.state().selected).toEqual([id])
+        })
+    
+        it('removes the id from the selected field when id is at the end of the array', () => {
+            const instance = wrapper.instance()
+            const expectedArr = props.users.map(user => user.id)
+            expectedArr.splice(-1, 1)
 
-        const selected = wrapper.state().selected
-        expect(selected.length).toEqual(props.users.length)
-        for(var i = 0; i < props.users.length; i++) {
-            expect(selected).toContain(props.users[i].id)
-        }
+            for(var i = 0; i < props.users.length; i++) {
+                instance.handleSelect(props.users[i].id)
+            }
+            instance.handleSelect(props.users[props.users.length - 1].id)
+
+            expect(wrapper.state().selected).toEqual(expectedArr)
+        })
     })
 })
