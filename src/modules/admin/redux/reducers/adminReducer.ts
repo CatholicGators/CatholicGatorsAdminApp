f5408a947c9a58b038b7aa907d4bb07197e4b0aa
Ryan Clements <ryanclementshax@gmail.com>
2019-09-21 15:55:40 -0400

    Add todo

--- src/modules/admin/redux/reducers/adminReducer.ts
+++ src/modules/admin/redux/reducers/adminReducer.ts
@@ -11,21 +11,22 @@ const applyUsersList = (state, action) => ({
 
 const applyUpdatedUser = (state, action) => ({
     ...state,
-    users: state.users.map(user => user.id === action.user.id ?
-        { ...action.user } :
-        user
+    users: state.users.map(user =>
+        user.id === action.user.id ? { ...action.user } : user
     )
 })
 
+// TODO: when we refactor this out, we should structure the reducers in the admin module like this: https://stackoverflow.com/a/42519797
 function adminReducer(state = INITIAL_ADMIN_STATE, action) {
-    switch(action.type) {
-        case adminActions.GET_USERS_SUCCESS : {
+    switch (action.type) {
+        case adminActions.GET_USERS_SUCCESS: {
             return applyUsersList(state, action)
         }
-        case adminActions.UPDATE_USER_SUCCESS : {
+        case adminActions.UPDATE_USER_SUCCESS: {
             return applyUpdatedUser(state, action)
         }
-        default : return state
+        default:
+            return state
     }
 }
 
